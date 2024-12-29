40c0017ac78b0525f491b6078daea2d034f9558c
cvpr25divla <cvpr25divla@gmail.com>
2024-12-29 15:02:37 +0800

    add title video2

--- change_video.py
+++ change_video.py
@@ -2,15 +2,15 @@ import os
 import subprocess
 
 # 设置文件夹路径，这里可以替换为你自己的文件夹路径
-folder_path = "/Users/lijinming/Downloads/select_viedo"
+folder_path = "/Users/lijinming/iCloud云盘（归档）/Desktop/学业/具身智能/project/coa_cvpr/static/videos"
 
 # 获取文件夹内所有MP4文件
 for filename in os.listdir(folder_path):
     if filename.endswith(".mp4"):
         # 构造完整文件路径
         input_file = os.path.join(folder_path, filename)
-        os.makedirs("/Users/lijinming/Downloads/select_viedo_mini", exist_ok=True)
-        output_file = os.path.join("/Users/lijinming/Downloads/select_viedo_mini",  filename)
+        os.makedirs("/Users/lijinming/iCloud云盘（归档）/Desktop/学业/具身智能/project/coa_cvpr/static/videos2", exist_ok=True)
+        output_file = os.path.join("/Users/lijinming/iCloud云盘（归档）/Desktop/学业/具身智能/project/coa_cvpr/static/videos2",  filename)
         if os.path.exists(output_file):
             print(f"文件已存在: {output_file}")
             continue
